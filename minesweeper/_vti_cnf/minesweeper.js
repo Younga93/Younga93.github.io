4c3795a847a22429192c96a6c996a38a6a712acc
Younga Jin <youngajin93@gmail.com>
2020-07-16 00:08:32 -0400

    Changed style
    >add a bootstrap(twitter)
    >add an picture of me! (jumping!)

---
+++ minesweeper/_vti_cnf/minesweeper.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|10 Jul 2020 04:25:31 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-DED1P78\\young
+vti_modifiedby:SR|DESKTOP-DED1P78\\young
+vti_timecreated:TR|10 Jul 2020 04:25:31 -0000
+vti_cacheddtm:TX|10 Jul 2020 04:25:31 -0000
+vti_filesize:IR|1510
+vti_backlinkinfo:VX|minesweeper/minesweeper.html
